e2535b2101546df480632367dd7ac98d9a9e4265
einspirell <71028312+einspirell@users.noreply.github.com>
2026-03-15 13:55:33 +0530

    Configure Next.js with experimental appDir support

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  experimental: { appDir: true },  // ✅ Add this line
+};
+
+module.exports = nextConfig;
